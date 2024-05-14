ea9041a64c6fbdcd920211ce872d2dfc7e325929
saii-d3v <saii.d3v@gmail.com>
2024-05-14 12:15:32 +0500

    link moved to outer scope

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,13 @@
 import time
 from selenium.webdriver.common.by import By
 
+
+link = "http://selenium1py.pythonanywhere.com/"
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
 
     time.sleep(2)
-    
+
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
