09dc21aa533b055b29474b2f7a30d2676c66bf2e
saii-d3v <saii.d3v@gmail.com>
2024-05-14 14:28:20 +0500

    code optimization

--- test_main_page.py
+++ test_main_page.py
@@ -6,8 +6,9 @@ link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
     browser.get(link)
-
     time.sleep(2)
+    go_to_login_page(browser)
 
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
