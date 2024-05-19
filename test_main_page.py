ddc95ca8df9a59ed87388edccf4f550c88251e02
saii-d3v <saii.d3v@gmail.com>
2024-05-19 18:58:47 +0500

    add logic for finding elements on page and create test to find login link

--- test_main_page.py
+++ test_main_page.py
@@ -7,5 +7,12 @@ def test_guest_can_go_to_login_page(browser):
     url = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, url)
     page.open()
-    time.sleep(2)
+    # time.sleep(2)
     page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
+    page.open()
+    # time.sleep(2)
+    page.should_be_login_link()
