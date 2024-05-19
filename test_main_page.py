f61dbfab68507b0b758f948cba88ad815097760b
saii-d3v <saii.d3v@gmail.com>
2024-05-19 21:27:17 +0500

    create login page class and format code

--- test_main_page.py
+++ test_main_page.py
@@ -1,18 +1,20 @@
 import time
 
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
+url = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    url = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, url)
     page.open()
-    # time.sleep(2)
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
 
 def test_guest_should_see_login_link(browser):
-    url = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, url)
     page.open()
-    # time.sleep(2)
     page.should_be_login_link()
